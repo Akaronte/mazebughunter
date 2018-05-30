b93264710904582c3969a835f02652e6d0296ff5
Ak <truefreedomandroid@gmail.com>
2018-05-30 20:14:55 +0200

    v0.1.2

--- gdx-game/src/org/barbasman/mazebughunter/MyGdxGame.java
+++ gdx-game/src/org/barbasman/mazebughunter/MyGdxGame.java
@@ -4,46 +4,25 @@ import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.*;
 
-public class MyGdxGame implements ApplicationListener
-{
-    Texture texture;
-    SpriteBatch batch;
+import org.barbasman.mazebughunter.screens.MainGameScreen;
+import com.badlogic.gdx.Game;
 
+public class MyGdxGame extends Game
+{
+   
+    public static final MainGameScreen _mainGameScreen = new MainGameScreen();
+    
     @Override
     public void create()
     {
-        texture = new Texture(Gdx.files.internal("android.jpg"));
-        batch = new SpriteBatch();
-    }
-
-    @Override
-    public void render()
-    {        
-        Gdx.gl.glClearColor(1, 1, 1, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(texture, Gdx.graphics.getWidth() / 4, 0, 
-                   Gdx.graphics.getWidth() / 2, Gdx.graphics.getWidth() / 2);
-        batch.end();
+        setScreen(_mainGameScreen);
     }
 
     @Override
     public void dispose()
     {
+        _mainGameScreen.dispose();
     }
 
-    @Override
-    public void resize(int width, int height)
-    {
-    }
-
-    @Override
-    public void pause()
-    {
-    }
-
-    @Override
-    public void resume()
-    {
-    }
+   
 }
